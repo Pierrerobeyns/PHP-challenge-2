35dba16bac4d79a836d1dd7915d5a838351f1cce
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-05-16 18:04:26 +0200

    query CompanyModel

--- model/CompaniesModel.php
+++ model/CompaniesModel.php
@@ -1,1 +1,45 @@
 <?php
+
+use app\model\Database;
+
+class ContactModel 
+{
+    public $db;
+
+    public function __construct()
+    {
+        return $this->db = new Database();
+    }
+
+    public function readAllCompaniesClients()
+    {
+        $query = 
+            "SELECT lastname, firstame, vatnumber, country 
+            FROM people 
+            INNER JOIN company 
+                on people.id = company.id 
+            INNER JOIN type 
+                on company.id = type.id 
+            WHERE type = 'Client'";
+        $stmt = $this->db->getInstance()->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+
+    public function readAllCompaniesSuppliers()
+    {
+        $query = 
+            "SELECT lastname, firstame, vatnumber, country 
+            FROM people 
+            INNER JOIN company 
+                on people.id = company.id 
+            INNER JOIN type 
+                on company.id = type.id 
+            WHERE type = 'Supplier'";
+        $stmt = $this->db->getInstance()->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+}
