3405600055c367e9994638fd775cdff3e07a8751
axel <axel.siriez@gmail.com>
2022-05-16 12:25:28 +0200

    login:squelette html

---
+++ view/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../assets/style/style.css">
+    <title>GOGIP - Login</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul class="nav-bar">
+                <li class="nav-item">COGIP</li>
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Invoices</li>
+                <li class="nav-item">Companies</li>
+                <li class="nav-item">Contact</li>
+            </ul>
+            <div class="nav-button">
+                <a href="#" class="sign-in">Sign in</a>
+                <a href="#" class="sign-up">Sign up</a>
+            </div>
+        </nav>
+    </header>
+</body>
+    <div>
+        <p class="welcome-msg">Welcome to the COGIP</p>
+        <p class="sub-welcome-msg">Bienvenue dans l’espace de connexion de la COGIP</p>
+    </div>
+    <form action="#" method="get">
+        <input type="text" name="id" placeholder="id">
+        <input type="text" name="password" placeholder="password"> 
+        <input type="submit" name="submit" value="Enter into the COGIP">
+    </form>
+
+</html>
