8252d243676814ef4bee96bc310d7229f4b8720d
axel <axel.siriez@gmail.com>
2022-05-16 15:35:49 +0200

    login page styling

--- view/login.php
+++ view/login.php
@@ -2,38 +2,43 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../assets/style/style.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="../assets/style/style.css" />
     <title>GOGIP - Login</title>
 </head>
 
 <body>
     <header>
         <nav>
-            <ul class="nav-bar">
-                <li class="nav-item">COGIP</li>
-                <li class="nav-item">Home</li>
-                <li class="nav-item">Invoices</li>
-                <li class="nav-item">Companies</li>
-                <li class="nav-item">Contact</li>
-            </ul>
+            <a href="#" class="nav-item">COGIP</a>
+            <div class="nav-bar">
+                <a href="#" class="nav-item">Home</a>
+                <a href="#" class="nav-item">Invoices</a>
+                <a href="#" class="nav-item">Companies</a>
+                <a href="#" class="nav-item">Contact</a>
+            </div>
             <div class="nav-button">
                 <a href="#" class="sign-in">Sign in</a>
                 <a href="#" class="sign-up">Sign up</a>
             </div>
         </nav>
     </header>
-</body>
-    <div>
-        <p class="welcome-msg">Welcome to the COGIP</p>
-        <p class="sub-welcome-msg">Bienvenue dans l’espace de connexion de la COGIP</p>
+    <div class="connect-section">
+        <div class="welcome-msg">
+            <p>Welcome to the COGIP</p>
+        </div>
+        <div class="sub-welcome-msg">
+            <p>Bienvenue dans l’espace de connexion de la COGIP</p>
+        </div>
+        <form action="#" method="get">
+            <input class="ipt-text" type="text" name="id" placeholder="id">
+            <input class="ipt-text" type="text" name="password" placeholder="password">
+            <input class="ipt-sub"  type="submit" name="submit" value="Enter into the COGIP">
+        </form>
     </div>
-    <form action="#" method="get">
-        <input type="text" name="id" placeholder="id">
-        <input type="text" name="password" placeholder="password"> 
-        <input type="submit" name="submit" value="Enter into the COGIP">
-    </form>
+
+</body>
 
 </html>
