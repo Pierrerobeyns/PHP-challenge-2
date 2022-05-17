6821804aa8a01104878afab918425cb2dfdc7cae
axel <axel.siriez@gmail.com>
2022-05-17 17:00:23 +0200

    error404 page

---
+++ view/error.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="nav-bar">
+                <a href="#" class="nav-item">COGIP</a>
+                <a href="#" class="nav-item">Home</a>
+                <a href="#" class="nav-item">Invoices</a>
+                <a href="#" class="nav-item">Companies</a>
+                <a href="#" class="nav-item">Contact</a>
+            </div>
+            <div class="nav-button">
+                <a href="#" class="sign-in">Sign in</a>
+                <a href="#" class="sign-up">Sign up</a>
+            </div>
+        </nav>
+    </header>
+
+</body>
+
+</html>
