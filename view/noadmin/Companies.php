49b4f05e030aa57f6664ffb884126bd7b1736455
axel <axel.siriez@gmail.com>
2022-05-23 14:15:02 +0200

    merge loic into axel

--- view/noadmin/Companies.php
+++ view/noadmin/Companies.php
@@ -1,59 +1,83 @@
-<h1>Companies directory</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<h3>Client</h3>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/style/header.css">
+    <title>Document</title>
+</head>
 
-<table>
-<thead>
-    <tr>
-        <th>Name</th>
-        <th>VAT number</th>
-        <th>Country</th>
-    </tr>
-</thead>
-<tbody>
-    <tr>
-        <?php foreach ($array1 as $elem) {
-            echo "<tr>";
-                echo "<td>";
-                echo $elem[0];
-                echo "</td>";
-                echo "<td>";
-                echo $elem[1];
-                echo "</td>";
-                echo "<td>";
-                echo $elem[2];
-                echo "</td>";
-            echo "<tr>";
-        } ; ?>
-    </tr>
-</tbody>
-</table>
+<body>
+    <?php
 
-<h3>Suppliers</h3>
+    use app\controller\Controller;
 
-<table>
-<thead>
-    <tr>
-        <th>Name</th>
-        <th>VAT number</th>
-        <th>Country</th>
-    </tr>
-</thead>
-<tbody>
-    <tr>
-        <?php foreach ($array2 as $elem) {
-            echo "<tr>";
-                echo "<td>";
-                echo $elem[0];
-                echo "</td>";
-                echo "<td>";
-                echo $elem[1];
-                echo "</td>";
-                echo "<td>";
-                echo $elem[2];
-                echo "</td>";
-            echo "<tr>";
-        } ; ?>
-    </tr>
-</tbody>
-</table>
+    $header = new Controller;
+    $header->view('./template/header');
+    ?>
+
+    <h1>Companies directory</h1>
+
+    <h3>Client</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>VAT number</th>
+                <th>Country</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php foreach ($array1 as $elem) {
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $elem[0];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $elem[1];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $elem[2];
+                    echo "</td>";
+                    echo "<tr>";
+                }; ?>
+            </tr>
+        </tbody>
+    </table>
+
+    <h3>Suppliers</h3>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>VAT number</th>
+                <th>Country</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php foreach ($array2 as $elem) {
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $elem[0];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $elem[1];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $elem[2];
+                    echo "</td>";
+                    echo "<tr>";
+                }; ?>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
