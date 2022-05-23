7449441db36c33e2f27a148df7060a9b48fea745
axel <axel.siriez@gmail.com>
2022-05-23 15:06:57 +0200

    companies page

--- view/noadmin/Companies.php
+++ view/noadmin/Companies.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/style/header.css">
+    <link rel="stylesheet" href="../../assets/style/Companies.css">
     <title>Document</title>
 </head>
 
@@ -20,63 +21,72 @@
 
     <h1>Companies directory</h1>
 
-    <h3>Client</h3>
+    <div class="table-pos">
+        <div class="table-styl">
+            <h3>Client</h3>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>VAT number</th>
-                <th>Country</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php foreach ($array1 as $elem) {
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $elem[0];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[1];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[2];
-                    echo "</td>";
-                    echo "<tr>";
-                }; ?>
-            </tr>
-        </tbody>
-    </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>VAT number</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php foreach ($array1 as $elem) {
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $elem[0];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[1];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[2];
+                            echo "</td>";
+                            echo "<tr>";
+                        }; ?>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-    <h3>Suppliers</h3>
+    <div class="table-pos">
+        <div class="table-styl">
 
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>VAT number</th>
-                <th>Country</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php foreach ($array2 as $elem) {
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $elem[0];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[1];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[2];
-                    echo "</td>";
-                    echo "<tr>";
-                }; ?>
-            </tr>
-        </tbody>
-    </table>
+            <h3>Suppliers</h3>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>VAT number</th>
+                        <th>Country</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php foreach ($array2 as $elem) {
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $elem[0];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[1];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[2];
+                            echo "</td>";
+                            echo "<tr>";
+                        }; ?>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 </body>
 
