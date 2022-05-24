4692ba7125421c45b5b10b76f6065e744fc45f77
axel <axel.siriez@gmail.com>
2022-05-24 10:23:11 +0200

    navbar link

--- view/template/header.php
+++ view/template/header.php
@@ -1,11 +1,12 @@
 <header>
     <nav>
         <div class="nav-bar">
-            <a href="../view/HomePage.php" class="nav-item"><img src="../assets/img/logo_cogip.png" alt="logo COGIP" width="50px"></a>
-            <a href="../view/HomePage.php" class="nav-item">Home</a>
-            <a href="#" class="nav-item">Invoices</a>
-            <a href="#" class="nav-item">Companies</a>
-            <a href="#" class="nav-item">Contact</a>
+            <a href="" class="nav-item"><img src="../assets/img/logo_cogip.png" alt="logo COGIP" width="50px"></a>
+            <a href="" class="nav-item">Home</a>
+            <a href="/noadmin/Invoices" class="nav-item">Invoices</a>
+            <a href="/noadmin/Companies" class="nav-item">Companies</a>
+            <a href="/noadmin/Contact" class="nav-item">Contact</a>
+
         </div>
         <div class="nav-button">
             <a href="../view/Registration.php" class="sign-in">Sign in</a>
