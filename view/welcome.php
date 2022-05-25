cee2efacc8d1aa4f9aed11c7aa42bd3b97446e93
axel <axel.siriez@gmail.com>
2022-05-25 10:53:12 +0200

    welcome table style

--- view/welcome.php
+++ view/welcome.php
@@ -46,107 +46,110 @@ if (!empty($_SESSION['GOD-MODE'])) {
         <a href="/admin/newcompanies"><img src="../assets/img/plus.svg" width="24px" />New Company</a>
     </div>
 
+    <div class="table-pos">
+        <div class="table-style">
 
-    <h1>Welcome</h1>
+            <h3>Last invoices :</h3>
 
-    <h3>Last invoices :</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Invoice number</th>
+                        <th>Dates</th>
+                        <th>Company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php foreach ($array1 as $elem) {
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $elem[0];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[1];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[2];
+                            echo "</td>";
+                            echo "<tr>";
+                        }; ?>
+                    </tr>
+                </tbody>
+            </table>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Invoice number</th>
-                <th>Dates</th>
-                <th>Company</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php foreach ($array1 as $elem) {
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $elem[0];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[1];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[2];
-                    echo "</td>";
-                    echo "<tr>";
-                }; ?>
-            </tr>
-        </tbody>
-    </table>
+            <h3>Last contacts :</h3>
 
-    <h3>Last contacts :</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Lastname</th>
+                        <th>Firstname</th>
+                        <th>Phone</th>
+                        <th>Mail</th>
+                        <th>Company</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php foreach ($array2 as $elem) {
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $elem[0];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[1];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[2];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[3];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[4];
+                            echo "</td>";
+                            echo "<tr>";
+                        }; ?>
+                    </tr>
+                </tbody>
+            </table>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Lastname</th>
-                <th>Firstname</th>
-                <th>Phone</th>
-                <th>Mail</th>
-                <th>Company</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php foreach ($array2 as $elem) {
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $elem[0];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[1];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[2];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[3];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[4];
-                    echo "</td>";
-                    echo "<tr>";
-                }; ?>
-            </tr>
-        </tbody>
-    </table>
 
-    <h3>Last Companies :</h3>
+            <h3>Last Companies :</h3>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Company</th>
-                <th>VAT number</th>
-                <th>Country</th>
-                <th>Type</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <?php foreach ($array3 as $elem) {
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $elem[0];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[1];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[2];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $elem[3];
-                    echo "</td>";
-                    echo "<tr>";
-                }; ?>
-            </tr>
-        </tbody>
-    </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Company</th>
+                        <th>VAT number</th>
+                        <th>Country</th>
+                        <th>Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php foreach ($array3 as $elem) {
+                            echo "<tr>";
+                            echo "<td>";
+                            echo $elem[0];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[1];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[2];
+                            echo "</td>";
+                            echo "<td>";
+                            echo $elem[3];
+                            echo "</td>";
+                            echo "<tr>";
+                        }; ?>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 
 </html>
