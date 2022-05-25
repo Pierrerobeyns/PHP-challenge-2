293de6aa7423acc5bb25fe7113468f7d47147459
axel <axel.siriez@gmail.com>
2022-05-25 10:21:13 +0200

    list.css & assets update

--- view/noadmin/Companies.php
+++ view/noadmin/Companies.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/style/header.css">
-    <link rel="stylesheet" href="../../assets/style/Companies.css">
+    <link rel="stylesheet" href="../../assets/style/List.css">
     <title>Document</title>
 </head>
 
